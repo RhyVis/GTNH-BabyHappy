bfcdea1b32ddef6f871c958e58fe64a8fb9944c9
RhyVis <vssr@foxmail.com>
2023-12-19 18:44:35 +0800

    Use ModifyReturnValue instead

--- src/main/java/com/rhynia/gtnh/gtbh/mixins/gtpp/IndustrialMultiMixin.java
+++ src/main/java/com/rhynia/gtnh/gtbh/mixins/gtpp/IndustrialMultiMixin.java
@@ -2,8 +2,8 @@ package com.rhynia.gtnh.gtbh.mixins.gtpp;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 
 import gtPlusPlus.xmod.gregtech.common.tileentities.machines.multi.processing.GregtechMetaTileEntity_IndustrialMultiMachine;
 
@@ -15,8 +15,8 @@ public class IndustrialMultiMixin {
      *
      * @since 1.0.1
      */
-    @Inject(method = "getMaxParallelRecipes", at = @At("RETURN"), cancellable = true)
-    private void bh$getMaxParallelRecipes(CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(cir.getReturnValue() * 4);
+    @ModifyReturnValue(method = "getMaxParallelRecipes", at = @At("RETURN"))
+    private int bh$getMaxParallelRecipes(int original) {
+        return original * 4;
     }
 }
